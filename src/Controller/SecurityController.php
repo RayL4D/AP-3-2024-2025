41d9cb7b52ae40c57aa85ebc2059deb2f9fcacce
Matheo <legoff.matheo4@gmail.com>
2024-11-05 14:07:19 +0100

    ok

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,9 +13,9 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
          if ($this->getUser()) {
-             return $this->redirectToRoute('security/base.html.twig');
+             return $this->render('security/base.html.twig');
          }
-
+        
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
